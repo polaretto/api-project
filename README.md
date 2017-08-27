ce6f51fa51e49d20121738b2cd3ec8565410ae4f
Ennio  Visconti <ennio.visconti@gmail.com>
2017-08-27 10:45:01 +0200

    Cleaned, commented and fixed a couple of things

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # api-project
 API Project - Report Generator
+
+## Usage
+`
+$ python api-report.py -i <input_file_name> -o <output_file_name> -b <http_url>
+`
+
+All parameters are optional.
+
+### Note:
+The script works only with a precise structure of the input csv (no header) and having the following fields in those specific positions:
+
+`
+<USER_CSV_ROW> : Username, _, _, Password, _*
+`
+
+It also relies on a specific HTML structure and may probably brake with CMS updates. 
--- api-report.py
+++ api-report.py
@@ -1,10 +1,13 @@
-import requests
 import csv
+import requests
+import argparse
+
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
-############################### SETTINGS ######################################
+
+########################### DEFAULT SETTINGS ##################################
 USERS_FILENAME = "users.csv"
 RESULTS_FILENAME = "results.csv"
 BASE_URL  = "https://dum-e.deib.polimi.it"
@@ -17,38 +20,72 @@ TASKS = [   "Tutorial",
             "Cactus",
             "Tiglio",
             "Potatura",
-            "Caccia_al_tesoro"]
+            "Caccia_al_tesoro" ]
 ###############################################################################
 
 def main( ) :
+    io = parameterHandler()
+
     headers = list(TASKS)
     headers.insert(0, "Username")
 
-    users = loadUsers(USERS_FILENAME)
-    results = fetchAllData(users)
-    storeResults(RESULTS_FILENAME, results, headers)
+    users = loadUsers(io['in'])
+    results = fetchingStrategy(users)
+    storeResults(io['out'], results, headers)
 
-def fetchAllData( users ):
+def parameterHandler():
+    global BASE_URL
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--input', help='Users csv file', required=False)
+    parser.add_argument('-o','--output', help='Results csv file', required=False)
+    parser.add_argument('-b','--base_url', help='The basic URL where the CMS is located', required=False)
+
+    args = parser.parse_args()
+    inputName = USERS_FILENAME
+    outputName = RESULTS_FILENAME
+
+    if args.input != None:
+        inputName = args.input
+    if args.output != None:
+        outputName = args.output
+    if args.base_url != None:
+        BASE_URL = args.base_url
+
+    return {'in' : inputName, 'out' : outputName}
+
+''' WARNING:
+    - fetchingStrategy: this method dramatically relies on the structure of the
+                        csv file. It takes as input a list  of <USR_TUPLE> and
+                        returns a dictionaries with the results for all
+                        the users in the input list.
+
+    <USR_TUPLE> : <Username, _, _, Password, _*> -> needed structure to work.
+'''
+def fetchingStrategy( users ):
     results = []
     count = len(users)
     for i,u in enumerate(users):
-        # print "Username: " + u[0] + " || Password: " + u[3]
         session = signIn(u[0], u[3])
-        # print "COOKIE: " + str(session)
-        r = fetchResults(session, TASKS)
+        r = fetchAllResults(session, TASKS)
         r.update({'Username' : u[0]})
         results.append(r)
         print str(i + 1) + "/" + str(count) + " done..."
-        #print "User <" + u[0] + "> completed!"
     return results
 
-def fetchResults( session, tasks ):
+def fetchAllResults( session, tasks ):
     results = {}
     for t in tasks:
         r = requests.get(TASK_URL(t), cookies=session)
         results.update({t : fetchLatestResult(r.text)})
     return results
 
+''' Traversing the document tree:
+    - fetchLatestResult:    strongly depends on the actual html structure we
+                            are trying to parse.
+                            Given an HTTP Response object, returns the list of
+                            scores of the user.
+'''
 def fetchLatestResult( html ):
     doc = BeautifulSoup(html, "html.parser")
     table = doc.find('tbody')
@@ -59,7 +96,7 @@ def fetchLatestResult( html ):
         score = row.find('td', {"class" : "public_score"})
         if score == None:
             return "No Submissions"
-    return score.text[1:-1].encode("utf-8")
+    return score.text.strip().encode("utf-8")
 
 def signIn( username, password ):
     payload = {'next': '/', 'username': username, 'password': password}
@@ -67,8 +104,16 @@ def signIn( username, password ):
     cookies = r.cookies;
     return cookies
 
+''' Input management methods:
+    - loadUsers:    loads the csv properly-formatted file and
+                    returns it into a list of tuples
+    - storeResults: stores the results list of dictionaries into a csv file
+                    having the same structure as the dictionaries
+
+    Note: these methods are csvs-structure invariant.
+'''
 def loadUsers( filename ):
-    with open('users.csv', 'rb') as f:
+    with open(filename, 'rb') as f:
         reader = csv.reader(f)
         users = map(tuple, reader)
     return users
@@ -81,4 +126,7 @@ def storeResults( filename, results, headers ):
             w.writerow(i)
     print "Results stored correctly at: " + filename
 
+'''
+    PROGRAM EXECUTION:
+'''
 main()
